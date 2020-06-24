3e5a6be5c418ee7267a6104ec1a357f18092bf37
clicker5@inbox.lv <clicker5@inbox.lv>
2020-06-24 18:25:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleAppExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person = new Human();
+            person.Name = "Ņiķita";
+            person.Surname = "Komars";
+            person.Age = 20;
+            person.EyeColor = "Brown";
+            person.Introduce();
+
+            person.Name = "Jānis";
+            person.Surname = "Roks";
+            person.Introduce();
+
+        }
+    }
+
+   
+}
